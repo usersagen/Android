1e78211d4142f2d95cd28846c71578bcaaf153f8
UsersAgen <2502124897@qq.com>
2025-12-29 23:41:10 +0800

    。

---
+++ app/src/main/java/com/example/hsj135/ui/video/VideoFragment.java
@@ -0,0 +1,37 @@
+package com.example.hsj135.ui.video;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.hsj135.databinding.FragmentVideoBinding;
+
+public class VideoFragment extends Fragment {
+
+    private FragmentVideoBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        VideoViewModel videoViewModel =
+                new ViewModelProvider(this).get(VideoViewModel.class);
+
+        binding = FragmentVideoBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textVideo;
+        videoViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
