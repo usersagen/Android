e5bc707fa1dafda22746d7fb200b03af01a411f7
UsersAgen <2502124897@qq.com>
2025-12-31 12:05:10 +0800

    。

--- app/src/main/java/com/example/hsj135/utils/NetUtils.java
+++ app/src/main/java/com/example/hsj135/utils/NetUtils.java
@@ -5,7 +5,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetUtils {
-    private static final String BASE_URL = "http://10.0.2.2:8080/topline/";
+    private static final String BASE_URL = "http://10.22.199.160:8080/topline/";
     public static GetRequest get() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
